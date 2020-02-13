1ec09f98b426f0e2092fe6bbf417b5d3cf24cdc6
Oleg_93 <vin76423@gmail.com>
2020-02-13 20:52:24 +0300

    seconde

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Здесь создаем конфигурационный файл, который дополняет main.py,
 # В модели mvc выполняет функцию "контроллер" или "модель" как и main.py
 class Configuration(object):
-    DEBUG=True
+    DEBUG=False
     
--- view.py
+++ view.py
@@ -3,6 +3,6 @@ from flask import render_template
 
 # фласк в данном случае выдает значение по ключу, где значение - это наша функц.отображения.
 # {'/' : 'view.index'} :
-@app.route('/')
+@app.route('page/')
 def index():
     return render_template('CSS_1.html')
